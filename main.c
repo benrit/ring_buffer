8c0419a21c5a528f47fdb21998994acf3924e433
benrit <ritterbwa@gmail.com>
2024-06-08 19:16:17 -0400

    initital commit

---
+++ main.c
@@ -0,0 +1,78 @@
+#include <stdint.h>
+#include <stdio.h>
+
+
+#define BUFFER_SIZE 1024
+
+
+typedef struct {
+    uint8_t buffer[BUFFER_SIZE];
+    uint32_t read;
+    uint32_t write;
+}RingBuffer;
+
+
+void ring_buffer_init(RingBuffer *rb){
+	rb->write = 1;
+	rb->read = 0;
+}
+
+uint32_t ring_buffer_enqueue(RingBuffer *rb, uint8_t data){
+    uint32_t temp = (rb->write + 1) % BUFFER_SIZE;
+    if (rb->read == rb->write){
+        return 0;
+    }
+
+    rb->buffer[rb->write] = data;
+    rb->write = temp;
+
+    return 1;
+}
+
+uint32_t ring_buffer_dequeue(RingBuffer *rb, uint8_t *data){
+
+    uint32_t temp = (rb->read + 1) % BUFFER_SIZE;
+
+    if (temp == rb->write){
+        return 0;
+    }
+    *data = rb->buffer[temp];
+    rb->read = temp;
+
+    return 1;
+}
+
+uint32_t ring_buffer_peak(RingBuffer *rb, uint32_t *data){
+	*data = rb->buffer[(rb->write - 1) % BUFFER_SIZE];
+
+	return 1;
+}
+
+uint32_t ring_buffer_compare(RingBuffer *rb, const uint8_t *data, const uint32_t len){
+	uint32_t read = (rb->read + 1) % BUFFER_SIZE;
+	uint32_t i = len;
+
+	while(i){
+		if (rb->buffer[(read + i) % BUFFER_SIZE ] != data[i]) return 0;
+		i--;
+	}
+	return 1;
+}
+
+
+int main(void){
+
+    RingBuffer rb={0};
+    ring_buffer_init(&rb);
+
+    ring_buffer_enqueue(&rb, 5);
+    ring_buffer_enqueue(&rb, 2);
+    uint8_t temp = 0;
+    ring_buffer_dequeue(&rb, &temp);
+
+    uint8_t data[] = {5, 2};
+
+    printf("%i", ring_buffer_compare(&rb, data, 2));
+
+    return 0;
+}
